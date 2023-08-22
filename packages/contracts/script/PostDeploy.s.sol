fc83e7541d160481fb778d3118ad99f97fd3c8f8
Cody Hatfield <cody.hatfield@me.com>
2023-08-21 23:00:38 -0700

    Deploy to Op Goerli

--- packages/contracts/script/PostDeploy.s.sol
+++ packages/contracts/script/PostDeploy.s.sol
@@ -15,15 +15,26 @@ contract PostDeploy is Script {
         // Start broadcasting transactions from the deployer account
         vm.startBroadcast(deployerPrivateKey);
 
-        IWorld(worldAddress).geoweb_ProfileSystem_setName("Initial name");
-        IWorld(worldAddress).geoweb_ProfileSystem_setUrl("Initial url");
+        IWorld(worldAddress).geoweb_ProfileSystem_setName("Example World");
+        IWorld(worldAddress).geoweb_ProfileSystem_setUrl(
+            "https://geoweb.network"
+        );
         IWorld(worldAddress).geoweb_MediaGallerySyst_addToMediaGallery(
             MediaObjectData({
-                name: "Name",
+                name: "App Logo",
                 mediaType: MediaObjectType.Image,
-                contentHash: hex"001122",
-                contentSize: 0,
-                encodingFormat: EncodingFormat.Jpeg
+                contentHash: hex"e3010182041220ab3df0316897c82f117f079bcd3466d439a3a87eff3fdaf6f6ebdee27c315bf5",
+                contentSize: 33649,
+                encodingFormat: EncodingFormat.Png
+            })
+        );
+        IWorld(worldAddress).geoweb_MediaGallerySyst_addToMediaGallery(
+            MediaObjectData({
+                name: "Buddha",
+                mediaType: MediaObjectType.Model3D,
+                contentHash: hex"e3010182041220af0a748b2ccc91ee6ad3d7a6b75ab22a9418f2bdc8ee7e9cb2c6613036027027",
+                contentSize: 22712008,
+                encodingFormat: EncodingFormat.Usdz
             })
         );
 
