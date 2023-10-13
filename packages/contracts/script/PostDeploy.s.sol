73f243453d65969d0eda081582fa25d0ae2d66a0
Cody Hatfield <cody.hatfield@me.com>
2023-10-13 16:13:19 -0700

    Deploy to Sepolia and OP Goerli

--- packages/contracts/script/PostDeploy.s.sol
+++ packages/contracts/script/PostDeploy.s.sol
@@ -4,14 +4,23 @@ pragma solidity >=0.8.0;
 import {Script} from "forge-std/Script.sol";
 import {console} from "forge-std/console.sol";
 import {IWorld} from "../src/codegen/world/IWorld.sol";
+import {PCOOwnershipModule} from "../src/modules/pcoownership/PCOOwnershipModule.sol";
 
 contract PostDeploy is Script {
     function run(address worldAddress) external {
+        IWorld world = IWorld(worldAddress);
+
         // Load the private key from the `PRIVATE_KEY` environment variable (in .env)
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
 
+        address parcelLicenseAddress = vm.envAddress("PARCEL_LICENSE_ADDRESS");
+
         // Start broadcasting transactions from the deployer account
         vm.startBroadcast(deployerPrivateKey);
+
+        // Install PCOOwnershipModule
+        PCOOwnershipModule module = new PCOOwnershipModule();
+        world.installRootModule(module, abi.encode(parcelLicenseAddress));
 
         vm.stopBroadcast();
     }
