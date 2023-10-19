f93cd4e447ba527467f11cdf00f2b2dd745f90d3
Cody Hatfield <cody.hatfield@me.com>
2023-10-18 18:54:38 -0700

    feat: add converters for parcel table IDs

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,12 +1,18 @@
 import { useComponentValue, useEntityQuery } from "@latticexyz/react";
-import { useMUD } from "@geo-web/mud-world-base-setup";
+import { useMUD, getComponentForParcel } from "@geo-web/mud-world-base-setup";
 import { singletonEntity } from "@latticexyz/store-sync/recs";
 import { Has, getComponentValueStrict } from "@latticexyz/recs";
+import mudConfig from "@geo-web/mud-world-base-contracts/mud.config";
 
 export const App = () => {
   const {
     components: { NameComponent },
   } = useMUD();
+
+  const nameComponent = getComponentForParcel(NameComponent, 320);
+  const nameComponents = useEntityQuery([Has(nameComponent)]);
+
+  console.log(nameComponents);
 
   return <>Hello World</>;
 };
--- packages/setup/src/mud/setupNetwork.ts
+++ packages/setup/src/mud/setupNetwork.ts
@@ -1,15 +1,65 @@
-import { createPublicClient, fallback, webSocket, http, createWalletClient, Hex, parseEther, ClientConfig, Chain } from "viem";
+import {
+  createPublicClient,
+  fallback,
+  webSocket,
+  http,
+  createWalletClient,
+  Hex,
+  encodePacked,
+  ClientConfig,
+  Chain,
+  stringToHex,
+  concatHex,
+  keccak256,
+  slice,
+} from "viem";
 import { createFaucetService } from "@latticexyz/services/faucet";
 import { encodeEntity, syncToRecs } from "@latticexyz/store-sync/recs";
 import { getNetworkConfig } from "./getNetworkConfig";
 import { world } from "./world";
 import { IWorld__factory } from "@geo-web/mud-world-base-contracts";
-import { createBurnerAccount, getContract, transportObserver, ContractWrite } from "@latticexyz/common";
+import {
+  createBurnerAccount,
+  getContract,
+  transportObserver,
+  ContractWrite,
+  resourceIdToHex,
+  resourceTypeIds,
+  hexToResourceId,
+} from "@latticexyz/common";
 import { Subject, share } from "rxjs";
 import mudConfig from "@geo-web/mud-world-base-contracts/mud.config";
 import { MUDChain } from "@latticexyz/common/chains";
+import { Component, Schema } from "@latticexyz/recs";
 
 export type SetupNetworkResult = Awaited<ReturnType<typeof setupNetwork>>;
+
+export function getTableIdForParcel(parcelId: bigint, tableName: string): Hex {
+  const typeId = resourceTypeIds.namespace;
+  return concatHex([
+    stringToHex(typeId, { size: 2 }),
+    slice(keccak256(encodePacked(["uint256"], [parcelId])), 1, 14),
+    stringToHex(tableName.slice(0, 16), { size: 16 }),
+  ]);
+}
+
+export function getTableIdsForParcel(mudConfig: any, parcelId: bigint): Hex[] {
+  const tableNames = Object.keys(mudConfig.tables).filter((key) =>
+    key.endsWith("Component")
+  );
+  return tableNames.map((tableName) => {
+    return getTableIdForParcel(parcelId, tableName);
+  });
+}
+
+export function getComponentForParcel<T extends Schema>(
+  component: Component<T>,
+  parcelId: bigint
+): Component<T> {
+  const resource = hexToResourceId(component.id as any);
+  const tableId = getTableIdForParcel(parcelId, resource.name);
+  return {...component, id: tableId};
+}
 
 export async function setupNetwork(networkParams: {
   chainId: Number;
@@ -41,13 +91,17 @@ export async function setupNetwork(networkParams: {
     onWrite: (write) => write$.next(write),
   });
 
-  const { components, latestBlock$, storedBlockLogs$, waitForTransaction } = await syncToRecs({
-    world,
-    config: mudConfig,
-    address: networkConfig.worldAddress as Hex,
-    publicClient,
-    startBlock: BigInt(networkConfig.initialBlockNumber),
-  });
+  const { components, latestBlock$, storedBlockLogs$, waitForTransaction } =
+    await syncToRecs({
+      world,
+      config: mudConfig,
+      address: networkConfig.worldAddress as Hex,
+      publicClient,
+      startBlock: BigInt(networkConfig.initialBlockNumber),
+      // tableIds: getTableIdsForParcel(mudConfig, 320n),
+    });
+
+    // console.log(getTableIdsForParcel(mudConfig, 320n))
 
   // try {
   //   console.log("creating faucet client");
@@ -68,7 +122,10 @@ export async function setupNetwork(networkParams: {
   return {
     world,
     components,
-    playerEntity: encodeEntity({ address: "address" }, { address: burnerWalletClient.account.address }),
+    playerEntity: encodeEntity(
+      { address: "address" },
+      { address: burnerWalletClient.account.address }
+    ),
     publicClient,
     walletClient: burnerWalletClient,
     latestBlock$,
