18357cb7e7f5ecf8f7d854dd5599c451f062b853
tnrdd <tnrdd@tutanota.com>
2023-11-22 18:26:21 +0000

    feat: add image augment (#6)
    * feat: add image augment
    
    * refactor: more precise geohash in installation script

--- packages/augments/script/InstallImageAugment.s.sol
+++ packages/augments/script/InstallImageAugment.s.sol
@@ -2,22 +2,22 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
-import "../src/ModelAugment.sol";
+import "../src/ImageAugment.sol";
 import {IWorld} from "@geo-web/mud-world-base-contracts/src/codegen/world/IWorld.sol";
 import {AugmentComponentValue} from "@geo-web/mud-world-base-contracts/src/modules/augmentinstallation/AugmentInstallationSystem.sol";
-import {NameCom, ModelCom} from "@geo-web/mud-world-base-contracts/src/codegen/index.sol";
-import {ModelEncodingFormat} from "@geo-web/mud-world-base-contracts/src/codegen/common.sol";
+import {NameCom, ImageCom} from "@geo-web/mud-world-base-contracts/src/codegen/index.sol";
+import {ImageEncodingFormat} from "@geo-web/mud-world-base-contracts/src/codegen/common.sol";
 import {PackedCounter} from "@latticexyz/store/src/PackedCounter.sol";
 import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 import {ResourceId, WorldResourceIdLib, WorldResourceIdInstance} from "@latticexyz/world/src/WorldResourceId.sol";
 import {RESOURCE_TABLE} from "@latticexyz/world/src/worldResourceTypes.sol";
 import "forge-std/console.sol";
 import {IAugment} from "@geo-web/mud-world-base-contracts/src/modules/augmentinstallation/IAugment.sol";
-import {NameCom, ModelCom, PositionCom, OrientationCom, ScaleCom} from "@geo-web/mud-world-base-contracts/src/codegen/index.sol";
+import {NameCom, ImageCom, PositionCom, OrientationCom, ScaleCom} from "@geo-web/mud-world-base-contracts/src/codegen/index.sol";
 
-contract InstallAugment is Script {
-    ModelAugment modelAugment =
-        ModelAugment(0x0c2819e12c930089D0F563467Fa6af4f87563019);
+contract InstallImageAugment is Script {
+    ImageAugment imageAugment =
+        ImageAugment(0x31897D284D8D7FD36335E5bc284af682d8174e31);
 
     IWorld world = IWorld(0x3904285496739BF5030d79C0CF259A569806F759);
 
@@ -29,15 +29,16 @@ contract InstallAugment is Script {
             memory componentValues = new AugmentComponentValue[][](1);
         componentValues[0] = new AugmentComponentValue[](5);
 
-        // Create ModelAugment
+        // Create ImageAugment
         {
             (
                 bytes memory staticData,
                 PackedCounter encodedLengths,
                 bytes memory dynamicData
-            ) = ModelCom.encode(
-                    ModelEncodingFormat.Glb,
-                    "ipfs://bafybeihgekplpvpizo3wwk3rpfnfsovx5k63ioxcxix7dinxuzng74sxiy"
+            ) = ImageCom.encode(
+                    ImageEncodingFormat.Jpeg,
+                    500,
+                    "ipfs://QmNiCFEQ8JeLjKW6UP2FDP3N7jU4RAGvZfPXExxMxjj5nd"
                 );
 
             componentValues[0][0] = AugmentComponentValue({
@@ -51,7 +52,7 @@ contract InstallAugment is Script {
                 bytes memory staticData,
                 PackedCounter encodedLengths,
                 bytes memory dynamicData
-            ) = NameCom.encode("Geo Web Logo");
+            ) = NameCom.encode("Lake");
 
             componentValues[0][1] = AugmentComponentValue({
                 staticData: staticData,
@@ -64,7 +65,7 @@ contract InstallAugment is Script {
                 bytes memory staticData,
                 PackedCounter encodedLengths,
                 bytes memory dynamicData
-            ) = PositionCom.encode(2000, "gcpvj335pbg");
+            ) = PositionCom.encode(1000, "dr72hbyjx00");
 
             componentValues[0][2] = AugmentComponentValue({
                 staticData: staticData,
@@ -105,16 +106,16 @@ contract InstallAugment is Script {
 
         world.grantAccess(
             WorldResourceIdLib.encodeNamespace(parcelNamespace),
-            address(modelAugment)
+            address(imageAugment)
         );
         world.installAugment(
-            modelAugment,
+            imageAugment,
             parcelNamespace,
             abi.encode(componentValues)
         );
         world.revokeAccess(
             WorldResourceIdLib.encodeNamespace(parcelNamespace),
-            address(modelAugment)
+            address(imageAugment)
         );
 
         vm.stopBroadcast();
